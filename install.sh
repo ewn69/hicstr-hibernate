cdf89f3db8fb0c93e66f4e49206ad1486f048834
ewn <83999562+ewn69@users.noreply.github.com>
2023-08-24 19:10:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ fi
 echo "Setting up Hicstr Sexy Hibernation for Paper no cap 2023 fire fire sex sex..."
 
 curl -L -o /var/lib/pterodactyl/volumes/script.sh https://raw.githubusercontent.com/ewn69/hicstr-hibernate/main/assets/script.sh
-curl -L -o /var/lib/pterodactyl/volumes/HicstrHibernate.jar https://raw.githubusercontent.com/ewn69/hicstr-hibernate/main/assets/HicstrHibernate.jar
+curl -L -o /var/lib/pterodactyl/volumes/HibernateX.jar https://raw.githubusercontent.com/ewn69/hicstr-hibernate/main/assets/HibernateX.jar
 sudo chmod -R 777 /var/lib/pterodactyl/volumes
 nohup /var/lib/pterodactyl/volumes/script.sh > /dev/null 2>&1 & 
 
